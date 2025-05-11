fd74da2ee59ec8baa4656a0f598b839c0e704398
rorybateman <rjb120@ic.ac.uk>
2025-05-11 13:24:48 +1000

    Moved build proceess inside docker file

--- agentbuild.sh
+++ agentbuild.sh
@@ -19,6 +19,4 @@ ros2 topic ls
 ros2 run micro_ros_setup create_agent_ws.sh
 
 ros2 run micro_ros_setup build_agent.sh
-source install/local_setup.bash
 
-ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyACM0
